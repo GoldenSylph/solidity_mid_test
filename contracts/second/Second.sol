bbf578bde7442300363b89e0afbca526efc8bdb1
Oleg Bedrin <simplavero@gmail.com>
2022-02-07 00:56:45 +0300

    adding rlp lib

--- contracts/second/Second.sol
+++ contracts/second/Second.sol
@@ -1,18 +1,17 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Context.sol";
+import "./RLPEncode.sol";
 
-contract Second is Context {
+contract Second {
 
     struct BlockHeader {
         bytes32 parentHash;
         bytes32 ommersHash;
         address beneficiary;
         bytes32 stateRoot;
-        bytes32 transactionsRoot;
         bytes32 receiptsRoot;
-        bytes logsBloom;
+        bytes32 logsBloom;
         uint256 difficulty;
         uint256 number;
         uint256 gasLimit;
@@ -23,31 +22,38 @@ contract Second is Context {
         uint64 nonce;
     }
 
-    function checkBlockHeader(BlockHeader calldata blockHeader, uint256 blockNumber) external pure returns(bool) {
+    function checkBlockHeader(BlockHeader calldata blockHeader, uint256 blockNumber) external view returns(bool) {
         bytes32 expectedBlockHash = blockhash(blockNumber);
-        bytes32 actualBlockHash = keccak256(abi.encode(
-          (
-            "bytes32", "bytes32", "address", "bytes32", "bytes32", "bytes32", "bytes",
-            "uint256", "uint256", "uint256", "uint256", "uint256",
-            "bytes", "bytes32", "uint64"
-          ),
-          blockHeader.parentHash,
-          blockHeader.ommersHash,
-          blockHeader.beneficiary,
-          blockHeader.stateRoot,
-          blockHeader.transactionsRoot,
-          blockHeader.receiptsRoot,
-          blockHeader.logsBloom,
-          blockHeader.difficulty,
-          blockHeader.number,
-          blockHeader.gasLimit,
-          blockHeader.gasUsed,
-          blockHeader.timestamp,
-          blockHeader.extraData,
-          blockHeader.mixHash,
-          blockHeader.nonce
-        ));
-        return expectedBlockHash == actualBlockHash;
+
+        // (
+        //   "bytes32", "bytes32", "address", "bytes32", "bytes32", "bytes32", "bytes",
+        //   "uint256", "uint256", "uint256", "uint256", "uint256",
+        //   "bytes", "bytes32", "uint64"
+        // ),
+        {
+            bytes memory encoded = abi.encode(
+              blockHeader.parentHash,
+              blockHeader.ommersHash,
+              blockHeader.beneficiary,
+              blockHeader.stateRoot,
+              blockHeader.transactionsRoot,
+              blockHeader.receiptsRoot,
+              blockHeader.logsBloom,
+              blockHeader.difficulty,
+              blockHeader.number,
+              blockHeader.gasLimit,
+              blockHeader.gasUsed,
+              blockHeader.timestamp,
+              blockHeader.extraData,
+              blockHeader.mixHash,
+              blockHeader.nonce
+            );
+            bytes32 actualBlockHash = keccak256(
+                encoded
+            );
+        }
+
+        return false;// expectedBlockHash == actualBlockHash;
     }
 
 }
